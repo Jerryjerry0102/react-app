5859896c3c8b2f2900bbf943a3ed89b31a479fcc
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-10-15 19:48:51 +0900

    nomad React #6 EFFECTS useEffect

---
+++ react-app-for-beginners/src/App.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prevState) => prevState + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  useEffect(() => console.log("I run only once."), []);
+  useEffect(() => console.log("I run when 'keyword' changes."), [keyword]);
+  useEffect(() => console.log("I run when 'counter' changes."), [counter]);
+  useEffect(
+    () => console.log("I run when 'keyword & counter' changes."),
+    [keyword, counter]
+  );
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
+
+// #5 CREATE REACT APP (css 관련)
+// import Button from "./Button";
+// import styles from "./App.module.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 className={styles.title}>Welcome back!</h1>
+//       <Button text={"continue"} />
+//     </div>
+//   );
+// }
+
+// export default App;
