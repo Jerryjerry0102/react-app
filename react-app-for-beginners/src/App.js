319b12743bcbafd5c7d02a58f6af708b6b598bfe
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-10-15 20:39:12 +0900

    nomad react #6 EFFECTS useEffect cleanUp function complete

--- react-app-for-beginners/src/App.js
+++ react-app-for-beginners/src/App.js
@@ -1,33 +1,57 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prevState) => prevState + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  useEffect(() => console.log("I run only once."), []);
-  useEffect(() => console.log("I run when 'keyword' changes."), [keyword]);
-  useEffect(() => console.log("I run when 'counter' changes."), [counter]);
-  useEffect(
-    () => console.log("I run when 'keyword & counter' changes."),
-    [keyword, counter]
-  );
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prevState) => !prevState);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
 
 export default App;
+
+// #6 EFFECTS (useEffect 관련)
+// import { useState, useEffect } from "react";
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const [keyword, setKeyword] = useState("");
+//   const onClick = () => setCounter((prevState) => prevState + 1);
+//   const onChange = (e) => setKeyword(e.target.value);
+//   useEffect(() => console.log("I run only once."), []);
+//   useEffect(() => console.log("I run when 'keyword' changes."), [keyword]);
+//   useEffect(() => console.log("I run when 'counter' changes."), [counter]);
+//   useEffect(
+//     () => console.log("I run when 'keyword & counter' changes."),
+//     [keyword, counter]
+//   );
+
+//   return (
+//     <div>
+//       <input
+//         value={keyword}
+//         onChange={onChange}
+//         type="text"
+//         placeholder="Search here."
+//       />
+//       <h1>{counter}</h1>
+//       <button onClick={onClick}>click me</button>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // #5 CREATE REACT APP (css 관련)
 // import Button from "./Button";
